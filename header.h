59f06a3a382ea6163e7d994c65e9289455114a7c
Augustas Gecevičius <agecev@gmail.com>
2026-04-23 20:27:54 +0300

    Derived klase

--- header.h
+++ header.h
@@ -30,6 +30,49 @@ public:
     }
 
 };
+class Studentas : public Zmogus {
+private : 
+    double egzaminas_;
+    std::vector<double> nd_;
+public:
+    Studentas() : Zmogus(), egzaminas_(0){}
+    Studentas(std::istream& is);
+    // konstruktorius su parametrais
+    Studentas(const std::string& vardas, const std::string& pavarde, int egzaminas, const std::vector<double>& nd);
+    //getters
+    inline int egzaminas() const {return egzaminas_;}
+    inline std::vector<double> nd() const {return nd_;}
+    //skaiciavimai
+    double galutBalas(double (*f) (std::vector<double>) = mediana) const;
+    std::istream& readStudent(std::istream& is); 
+    //setters
+    void setEgz(double egzam){egzaminas_ = egzam;}
+    void setNd(double x){nd_.push_back(x);}    
+
+    // ivestis, isvestis
+    friend std::istream& operator>>(std::istream& is, Studentas& s);
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
+
+    // rule of five
+    Studentas(const Studentas& other); // copy constructor
+
+    Studentas(Studentas&& other) noexcept; // move constructor
+
+    Studentas& operator=(const Studentas& other); // priskyrimo kopijavimo operatorius
+
+    Studentas& operator=(Studentas&& other) noexcept; // priskyrimo move operatorius
+
+    ~Studentas() {
+    vardas_.clear();
+    pavarde_.clear();
+    nd_.clear();
+    egzaminas_=0;
+    std::cout<<"Destruktorius \n";
+    }// destructor
+};
+
+
+/*
 class Studentas {
 private:
 std::string vardas_;
@@ -77,7 +120,7 @@ egzaminas_=0;
 std::cout<<"Destruktorius \n";
 }// destructor
 
-};
+};*/
 
 bool compare(const Studentas& a, const Studentas& b);
 bool comparePagalPavarde(const Studentas& a, const Studentas& b);
